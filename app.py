cb21d0ce3c1776d779ee368362801249e751862a
giwhanrhee-creator <giwhanrhee@gmail.com>
2026-05-28 18:30:30 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ if st.button("구글과 연결 테스트 시작"):
         st.write("1. Secrets에서 키를 불러오는 데 성공했습니다.")
         
         genai.configure(api_key=api_key)
-        model = genai.GenerativeModel("gemini-1.5-flash")
+        model = genai.GenerativeModel("gemini-1.0-pro")
         
         st.write("2. 구글 AI 모델에 연결 중...")
         response = model.generate_content("안녕? 연결되었니?")
